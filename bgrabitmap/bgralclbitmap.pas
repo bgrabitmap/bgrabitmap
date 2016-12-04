199452d07dff3aa4609461883c6882af4b9a18b9
Johann <circular@operamail.com>
2016-12-04 11:39:20 +0100

    support for 1 bit raw image

--- bgrabitmap/bgralclbitmap.pas
+++ bgrabitmap/bgralclbitmap.pas
@@ -58,6 +58,88 @@ uses BGRAText, LCLType, LCLIntf, FPimage;
 type
   TCopyPixelProc = procedure (psrc: PByte; pdest: PBGRAPixel; count: NativeInt; sourcePixelSize: PtrInt; defaultOpacity: byte);
 
+procedure ApplyMask1bit(psrc: PByte; pdest: PBGRAPixel; count: NativeInt; {%H-}sourcePixelSize: PtrInt; {%H-}defaultOpacity: byte);
+var currentBit: byte;
+begin
+  currentBit := 1;
+  while count > 0 do
+  begin
+    if psrc^ and currentBit <> 0 then
+      pdest^.alpha := 0;
+    inc(pdest);
+    if currentBit = 128 then
+    begin
+      currentBit := 1;
+      inc(psrc);
+    end else
+      currentBit := currentBit shl 1;
+    dec(count);
+  end;
+end;
+
+procedure ApplyMask1bitRev(psrc: PByte; pdest: PBGRAPixel; count: NativeInt; {%H-}sourcePixelSize: PtrInt; {%H-}defaultOpacity: byte);
+var currentBit: byte;
+begin
+  currentBit := 128;
+  while count > 0 do
+  begin
+    if psrc^ and currentBit <> 0 then
+      pdest^.alpha := 0;
+    inc(pdest);
+    if currentBit = 1 then
+    begin
+      currentBit := 128;
+      inc(psrc);
+    end else
+      currentBit := currentBit shr 1;
+    dec(count);
+  end;
+end;
+
+procedure CopyFromBW_SetAlpha(psrc: PByte; pdest: PBGRAPixel; count: NativeInt; {%H-}sourcePixelSize: PtrInt; defaultOpacity: byte);
+var currentBit: byte;
+begin
+  currentBit := 1;
+  while count > 0 do
+  begin
+    if psrc^ and currentBit <> 0 then
+      pdest^ := BGRAWhite
+    else
+      pdest^ := BGRABlack;
+    pdest^.alpha := DefaultOpacity;
+    inc(pdest);
+    if currentBit = 128 then
+    begin
+      currentBit := 1;
+      inc(psrc);
+    end else
+      currentBit := currentBit shl 1;
+    dec(count);
+  end;
+end;
+
+procedure CopyFromBW_SetAlphaBitRev(psrc: PByte; pdest: PBGRAPixel; count: NativeInt; {%H-}sourcePixelSize: PtrInt; defaultOpacity: byte);
+var currentBit: byte;
+begin
+  currentBit := 128;
+  while count > 0 do
+  begin
+    if psrc^ and currentBit <> 0 then
+      pdest^ := BGRAWhite
+    else
+      pdest^ := BGRABlack;
+    pdest^.alpha := DefaultOpacity;
+    inc(pdest);
+    if currentBit = 1 then
+    begin
+      currentBit := 128;
+      inc(psrc);
+    end else
+      currentBit := currentBit shr 1;
+    dec(count);
+  end;
+end;
+
 procedure CopyFrom24Bit(psrc: PByte; pdest: PBGRAPixel; count: NativeInt; sourcePixelSize: PtrInt; defaultOpacity: byte);
 begin
   while count > 0 do
@@ -262,7 +344,6 @@ var
   psource_first, pdest_first: PByte;
   psource_delta, pdest_delta: integer;
 
-  n: integer;
   mustSwapRedBlue: boolean;
   copyProc: TCopyPixelProc;
   nbColorChannels: integer;
@@ -273,6 +354,45 @@ var
       raise Exception.Create('Invalid raw image format. ' + message)
     else
       result := false;
+  end;
+
+  procedure DoCopyProc(AData: PByte; ABytesPerLine, ABitsPerPixel: integer);
+  var n: integer;
+  begin
+    if (ARawImage.Description.LineOrder = ADestination.LineOrder) and
+      (ABytesPerLine = (ABitsPerPixel shr 3) * cardinal(ADestination.Width)) then
+      copyProc(AData, ADestination.Data, ADestination.NbPixels, ABitsPerPixel shr 3, DefaultOpacity)
+    else
+    begin
+      if ARawImage.Description.LineOrder = riloTopToBottom then
+      begin
+        psource_first := AData;
+        psource_delta := ABytesPerLine;
+      end else
+      begin
+        psource_first := AData + (ARawImage.Description.Height-1) * ABytesPerLine;
+        psource_delta := -ABytesPerLine;
+      end;
+
+      if ADestination.LineOrder = riloTopToBottom then
+      begin
+        pdest_first := PByte(ADestination.Data);
+        pdest_delta := ADestination.Width*sizeof(TBGRAPixel);
+      end else
+      begin
+        pdest_first := PByte(ADestination.Data) + (ADestination.Height-1)*ADestination.Width*sizeof(TBGRAPixel);
+        pdest_delta := -ADestination.Width*sizeof(TBGRAPixel);
+      end;
+
+      psource_byte := psource_first;
+      pdest_byte := pdest_first;
+      for n := ADestination.Height-1 downto 0 do
+      begin
+        copyProc(psource_byte, PBGRAPixel(pdest_byte), ADestination.Width, ABitsPerPixel shr 3, DefaultOpacity);
+        inc(psource_byte, psource_delta);
+        inc(pdest_byte, pdest_delta);
+      end;
+    end;
   end;
 
 begin
@@ -286,164 +406,149 @@ begin
     exit;
   end;
 
-  if ((ARawImage.Description.BitsPerPixel and 7) <> 0) then
+  if ARawImage.Description.BitsPerPixel = 1 then
   begin
-    result := FormatError(IntToStr(ARawImage.Description.Depth) + 'bit found but multiple of 8bit expected');
-    exit;
-  end;
-
-  if (ARawImage.Description.BitsPerPixel < 24) then
-  begin
-    result := FormatError(IntToStr(ARawImage.Description.Depth) + 'bit found but at least 24bit expected');
-    exit;
-  end;
-
-  nbColorChannels := 0;
-  if (ARawImage.Description.RedPrec > 0)  then inc(nbColorChannels);
-  if (ARawImage.Description.GreenPrec > 0)  then inc(nbColorChannels);
-  if (ARawImage.Description.BluePrec > 0)  then inc(nbColorChannels);
-
-  if (nbColorChannels < 3) then
-  begin
-    result := FormatError('One or more color channel is missing (RGB expected)');
-    exit;
-  end;
-
-  //channels are in ARGB order
-  if (ARawImage.Description.BitsPerPixel >= 32) and
-     (ARawImage.Description.AlphaPrec = 8) and
-    (((ARawImage.Description.AlphaShift = 0) and
-    (ARawImage.Description.RedShift = 8) and
-    (ARawImage.Description.GreenShift = 16) and
-    (ARawImage.Description.BlueShift = 24) and
-    (ARawImage.Description.ByteOrder = riboLSBFirst)) or
-    ((ARawImage.Description.AlphaShift = ARawImage.Description.BitsPerPixel - 8) and
-    (ARawImage.Description.RedShift = ARawImage.Description.BitsPerPixel - 16) and
-    (ARawImage.Description.GreenShift = ARawImage.Description.BitsPerPixel - 24) and
-    (ARawImage.Description.BlueShift = ARawImage.Description.BitsPerPixel - 32) and
-    (ARawImage.Description.ByteOrder = riboMSBFirst))) then
-    begin
-      if AlwaysReplaceAlpha then
-        copyProc := @CopyFromARGB_SetAlpha
-      else if DefaultOpacity = 0 then
-        copyProc := @CopyFromARGB_KeepAlpha
-      else
-        copyProc := @CopyFromARGB_ReplaceZeroAlpha;
-    end
-  else //channels are in ARGB order but alpha is not used
-  if (ARawImage.Description.BitsPerPixel >= 32) and
-     (ARawImage.Description.AlphaPrec = 0) and
-    (((ARawImage.Description.RedShift = 8) and
-    (ARawImage.Description.GreenShift = 16) and
-    (ARawImage.Description.BlueShift = 24) and
-    (ARawImage.Description.ByteOrder = riboLSBFirst)) or
-    ((ARawImage.Description.RedShift = ARawImage.Description.BitsPerPixel - 16) and
-    (ARawImage.Description.GreenShift = ARawImage.Description.BitsPerPixel - 24) and
-    (ARawImage.Description.BlueShift = ARawImage.Description.BitsPerPixel - 32) and
-    (ARawImage.Description.ByteOrder = riboMSBFirst))) then
-    begin
-      DefaultOpacity := 255;
-      copyProc := @CopyFromARGB_SetAlpha;
-    end
-  else
-  begin
-    //channels are in RGB order (alpha channel may follow)
-    if (ARawImage.Description.BitsPerPixel >= 24) and
-       (((ARawImage.Description.RedShift = 0) and
-         (ARawImage.Description.GreenShift = 8) and
-         (ARawImage.Description.BlueShift = 16) and
-         (ARawImage.Description.ByteOrder = riboLSBFirst)) or
-        ((ARawImage.Description.RedShift = ARawImage.Description.BitsPerPixel - 8) and
-         (ARawImage.Description.GreenShift = ARawImage.Description.BitsPerPixel - 16) and
-         (ARawImage.Description.BlueShift = ARawImage.Description.BitsPerPixel - 24) and
-         (ARawImage.Description.ByteOrder = riboMSBFirst))) then
-    begin
-      mustSwapRedBlue:= not TBGRAPixel_RGBAOrder;
-    end
+    if ARawImage.Description.BitOrder = riboBitsInOrder then
+      copyProc := @CopyFromBW_SetAlpha
     else
-    //channels are in BGR order (alpha channel may follow)
-    if (ARawImage.Description.BitsPerPixel >= 24) and
-       (((ARawImage.Description.BlueShift = 0) and
-         (ARawImage.Description.GreenShift = 8) and
-         (ARawImage.Description.RedShift = 16) and
-         (ARawImage.Description.ByteOrder = riboLSBFirst)) or
-        ((ARawImage.Description.BlueShift = ARawImage.Description.BitsPerPixel - 8) and
-         (ARawImage.Description.GreenShift = ARawImage.Description.BitsPerPixel - 16) and
-         (ARawImage.Description.RedShift = ARawImage.Description.BitsPerPixel - 24) and
-         (ARawImage.Description.ByteOrder = riboMSBFirst))) then
+      copyProc := @CopyFromBW_SetAlphaBitRev;
+    DefaultOpacity := 255;
+  end else
+  begin
+    if ((ARawImage.Description.BitsPerPixel and 7) <> 0) then
     begin
-      mustSwapRedBlue:= TBGRAPixel_RGBAOrder;
-    end
-    else
-    begin
-      result := FormatError('BitsPerPixel: ' + IntToStr(ARawImage.Description.BitsPerPixel) + ', '
-        + 'RedShit: ' + IntToStr(ARawImage.Description.RedShift) + ', Prec: ' + IntToStr(ARawImage.Description.RedPrec)+ ', '
-        + 'GreenShit: ' + IntToStr(ARawImage.Description.GreenShift) + ', Prec: ' + IntToStr(ARawImage.Description.GreenPrec)+ ', '
-        + 'BlueShift: ' + IntToStr(ARawImage.Description.BlueShift) + ', Prec: ' + IntToStr(ARawImage.Description.BluePrec)+ ', '
-        + 'AlphaShift: ' + IntToStr(ARawImage.Description.AlphaShift) + ', Prec: ' + IntToStr(ARawImage.Description.AlphaPrec) );
+      result := FormatError(IntToStr(ARawImage.Description.Depth) + 'bit found but multiple of 8bit expected');
       exit;
     end;
 
-    if not mustSwapRedBlue then
+    if (ARawImage.Description.BitsPerPixel < 24) then
     begin
-      if ARawImage.Description.BitsPerPixel = 24 then
-        copyProc := @CopyFrom24Bit
-      else
-      if AlwaysReplaceAlpha or (ARawImage.Description.AlphaPrec = 0) then
-        copyProc := @CopyFrom32Bit_SetAlpha
-      else if DefaultOpacity = 0 then
-        copyProc := @CopyFrom32Bit_KeepAlpha
-      else
-        copyProc := @CopyFrom32Bit_ReplaceZeroAlpha;
-    end else
+      result := FormatError(IntToStr(ARawImage.Description.Depth) + 'bit found but at least 24bit expected');
+      exit;
+    end;
+
+    nbColorChannels := 0;
+    if (ARawImage.Description.RedPrec > 0)  then inc(nbColorChannels);
+    if (ARawImage.Description.GreenPrec > 0)  then inc(nbColorChannels);
+    if (ARawImage.Description.BluePrec > 0)  then inc(nbColorChannels);
+
+    if (nbColorChannels < 3) then
     begin
-      if ARawImage.Description.BitsPerPixel = 24 then
-        copyProc := @CopyFrom24Bit_SwapRedBlue
+      result := FormatError('One or more color channel is missing (RGB expected)');
+      exit;
+    end;
+
+    //channels are in ARGB order
+    if (ARawImage.Description.BitsPerPixel >= 32) and
+       (ARawImage.Description.AlphaPrec = 8) and
+      (((ARawImage.Description.AlphaShift = 0) and
+      (ARawImage.Description.RedShift = 8) and
+      (ARawImage.Description.GreenShift = 16) and
+      (ARawImage.Description.BlueShift = 24) and
+      (ARawImage.Description.ByteOrder = riboLSBFirst)) or
+      ((ARawImage.Description.AlphaShift = ARawImage.Description.BitsPerPixel - 8) and
+      (ARawImage.Description.RedShift = ARawImage.Description.BitsPerPixel - 16) and
+      (ARawImage.Description.GreenShift = ARawImage.Description.BitsPerPixel - 24) and
+      (ARawImage.Description.BlueShift = ARawImage.Description.BitsPerPixel - 32) and
+      (ARawImage.Description.ByteOrder = riboMSBFirst))) then
+      begin
+        if AlwaysReplaceAlpha then
+          copyProc := @CopyFromARGB_SetAlpha
+        else if DefaultOpacity = 0 then
+          copyProc := @CopyFromARGB_KeepAlpha
+        else
+          copyProc := @CopyFromARGB_ReplaceZeroAlpha;
+      end
+    else //channels are in ARGB order but alpha is not used
+    if (ARawImage.Description.BitsPerPixel >= 32) and
+       (ARawImage.Description.AlphaPrec = 0) and
+      (((ARawImage.Description.RedShift = 8) and
+      (ARawImage.Description.GreenShift = 16) and
+      (ARawImage.Description.BlueShift = 24) and
+      (ARawImage.Description.ByteOrder = riboLSBFirst)) or
+      ((ARawImage.Description.RedShift = ARawImage.Description.BitsPerPixel - 16) and
+      (ARawImage.Description.GreenShift = ARawImage.Description.BitsPerPixel - 24) and
+      (ARawImage.Description.BlueShift = ARawImage.Description.BitsPerPixel - 32) and
+      (ARawImage.Description.ByteOrder = riboMSBFirst))) then
+      begin
+        DefaultOpacity := 255;
+        copyProc := @CopyFromARGB_SetAlpha;
+      end
+    else
+    begin
+      //channels are in RGB order (alpha channel may follow)
+      if (ARawImage.Description.BitsPerPixel >= 24) and
+         (((ARawImage.Description.RedShift = 0) and
+           (ARawImage.Description.GreenShift = 8) and
+           (ARawImage.Description.BlueShift = 16) and
+           (ARawImage.Description.ByteOrder = riboLSBFirst)) or
+          ((ARawImage.Description.RedShift = ARawImage.Description.BitsPerPixel - 8) and
+           (ARawImage.Description.GreenShift = ARawImage.Description.BitsPerPixel - 16) and
+           (ARawImage.Description.BlueShift = ARawImage.Description.BitsPerPixel - 24) and
+           (ARawImage.Description.ByteOrder = riboMSBFirst))) then
+      begin
+        mustSwapRedBlue:= not TBGRAPixel_RGBAOrder;
+      end
       else
-      if AlwaysReplaceAlpha or (ARawImage.Description.AlphaPrec = 0) then
-        copyProc := @CopyFrom32Bit_SwapRedBlue_SetAlpha
-      else if DefaultOpacity = 0 then
-        copyProc := @CopyFrom32Bit_SwapRedBlue_KeepAlpha
+      //channels are in BGR order (alpha channel may follow)
+      if (ARawImage.Description.BitsPerPixel >= 24) and
+         (((ARawImage.Description.BlueShift = 0) and
+           (ARawImage.Description.GreenShift = 8) and
+           (ARawImage.Description.RedShift = 16) and
+           (ARawImage.Description.ByteOrder = riboLSBFirst)) or
+          ((ARawImage.Description.BlueShift = ARawImage.Description.BitsPerPixel - 8) and
+           (ARawImage.Description.GreenShift = ARawImage.Description.BitsPerPixel - 16) and
+           (ARawImage.Description.RedShift = ARawImage.Description.BitsPerPixel - 24) and
+           (ARawImage.Description.ByteOrder = riboMSBFirst))) then
+      begin
+        mustSwapRedBlue:= TBGRAPixel_RGBAOrder;
+      end
       else
-        copyProc := @CopyFrom32Bit_SwapRedBlue_ReplaceZeroAlpha;
+      begin
+        result := FormatError('BitsPerPixel: ' + IntToStr(ARawImage.Description.BitsPerPixel) + ', '
+          + 'RedShit: ' + IntToStr(ARawImage.Description.RedShift) + ', Prec: ' + IntToStr(ARawImage.Description.RedPrec)+ ', '
+          + 'GreenShit: ' + IntToStr(ARawImage.Description.GreenShift) + ', Prec: ' + IntToStr(ARawImage.Description.GreenPrec)+ ', '
+          + 'BlueShift: ' + IntToStr(ARawImage.Description.BlueShift) + ', Prec: ' + IntToStr(ARawImage.Description.BluePrec)+ ', '
+          + 'AlphaShift: ' + IntToStr(ARawImage.Description.AlphaShift) + ', Prec: ' + IntToStr(ARawImage.Description.AlphaPrec) );
+        exit;
+      end;
+
+      if not mustSwapRedBlue then
+      begin
+        if ARawImage.Description.BitsPerPixel = 24 then
+          copyProc := @CopyFrom24Bit
+        else
+        if AlwaysReplaceAlpha or (ARawImage.Description.AlphaPrec = 0) then
+          copyProc := @CopyFrom32Bit_SetAlpha
+        else if DefaultOpacity = 0 then
+          copyProc := @CopyFrom32Bit_KeepAlpha
+        else
+          copyProc := @CopyFrom32Bit_ReplaceZeroAlpha;
+      end else
+      begin
+        if ARawImage.Description.BitsPerPixel = 24 then
+          copyProc := @CopyFrom24Bit_SwapRedBlue
+        else
+        if AlwaysReplaceAlpha or (ARawImage.Description.AlphaPrec = 0) then
+          copyProc := @CopyFrom32Bit_SwapRedBlue_SetAlpha
+        else if DefaultOpacity = 0 then
+          copyProc := @CopyFrom32Bit_SwapRedBlue_KeepAlpha
+        else
+          copyProc := @CopyFrom32Bit_SwapRedBlue_ReplaceZeroAlpha;
+      end;
     end;
   end;
 
-  if (ARawImage.Description.LineOrder = ADestination.LineOrder) and
-    (ARawImage.Description.BytesPerLine = (ARawImage.Description.BitsPerPixel shr 3) * cardinal(ADestination.Width)) then
-    copyProc(ARawImage.Data, ADestination.Data, ADestination.NbPixels, ARawImage.Description.BitsPerPixel shr 3, DefaultOpacity)
-  else
+  DoCopyProc(ARawImage.Data, ARawImage.Description.BytesPerLine, ARawImage.Description.BitsPerPixel);
+
+  if (ARawImage.Description.AlphaPrec = 0) and (ARawImage.Description.MaskBitsPerPixel = 1) then
   begin
-    if ARawImage.Description.LineOrder = riloTopToBottom then
-    begin
-      psource_first := ARawImage.Data;
-      psource_delta := ARawImage.Description.BytesPerLine;
-    end else
-    begin
-      psource_first := ARawImage.Data + (ARawImage.Description.Height-1) * ARawImage.Description.BytesPerLine;
-      psource_delta := -ARawImage.Description.BytesPerLine;
-    end;
-
-    if ADestination.LineOrder = riloTopToBottom then
-    begin
-      pdest_first := PByte(ADestination.Data);
-      pdest_delta := ADestination.Width*sizeof(TBGRAPixel);
-    end else
-    begin
-      pdest_first := PByte(ADestination.Data) + (ADestination.Height-1)*ADestination.Width*sizeof(TBGRAPixel);
-      pdest_delta := -ADestination.Width*sizeof(TBGRAPixel);
-    end;
-
-    psource_byte := psource_first;
-    pdest_byte := pdest_first;
-    for n := ADestination.Height-1 downto 0 do
-    begin
-      copyProc(psource_byte, PBGRAPixel(pdest_byte), ADestination.Width, ARawImage.Description.BitsPerPixel shr 3, DefaultOpacity);
-      inc(psource_byte, psource_delta);
-      inc(pdest_byte, pdest_delta);
-    end;
+    if ARawImage.Description.BitOrder = riboBitsInOrder then
+      copyProc := @ApplyMask1bit
+    else
+      copyProc := @ApplyMask1bitRev;
+    DoCopyProc(ARawImage.Mask, ARawImage.Description.MaskBytesPerLine, ARawImage.Description.MaskBitsPerPixel);
   end;
-
   ADestination.InvalidateBitmap;
   result := true;
 end;
