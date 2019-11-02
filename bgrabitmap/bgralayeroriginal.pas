dc7a8ec9c461c84f0dc26e78d3ea413f53f520dc
johann <circular@operamail.com>
2019-11-02 14:55:23 +0100

    special key str

--- bgrabitmap/bgralayeroriginal.pas
+++ bgrabitmap/bgralayeroriginal.pas
@@ -33,6 +33,18 @@ type
                  sk0, sk1, sk2, sk3, sk4, sk5, sk6, sk7, sk8, sk9,
                  skShift, skCtrl, skAlt);
 
+const
+  SpecialKeyStr: array[TSpecialKey] of string =
+    ('Unknown', 'Backspace', 'Tab', 'Return', 'Escape',
+     'PageUp', 'PageDown', 'Home', 'End',
+     'Left', 'Up', 'Right', 'Down',
+     'Insert', 'Delete',
+     'Num0', 'Num1', 'Num2', 'Num3', 'Num4', 'Num5', 'Num6', 'Num7', 'Num8', 'Num9',
+     'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12',
+     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+     'Shift', 'Ctrl', 'Alt');
+
 {$IFDEF BGRABITMAP_USE_LCL}
 const
   SpecialKeyToLCL: array[TSpecialKey] of Word =
