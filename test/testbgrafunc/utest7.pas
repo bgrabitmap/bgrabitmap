692da4500b91c56805fcf2c482ed61a4cf440f18
Johann <circular@operamail.com>
2020-05-03 13:27:28 +0200

    test DrawPixelF

--- test/testbgrafunc/utest7.pas
+++ test/testbgrafunc/utest7.pas
@@ -5,7 +5,7 @@ unit utest7;
 interface
 
 uses
-  Classes, SysUtils, utest, Graphics, BGRABitmap, BGRABitmapTypes, BGRAGradientScanner;
+  BGRAClasses, SysUtils, utest, Graphics, BGRABitmap, BGRABitmapTypes, BGRAGradientScanner;
 
 type
   { TTest7 }
@@ -15,12 +15,15 @@ type
     pacman,virtualScreen,background: TBGRABitmap;
     time: single;
     multigrad: TBGRACustomGradient;
+    pixels: array of record
+         x,y,dx,dy: single;
+      end;
     procedure DrawPacman(x,y: integer; angle: single);
   public
     constructor Create;
     destructor Destroy; override;
     procedure OnPaint(Canvas: TCanvas; Left,Top,Width,Height: Integer); override;
-    procedure OnTimer(Width,Height: Integer; ElapsedSec: Double); override;
+    procedure OnTimer({%H-}Width,{%H-}Height: Integer; ElapsedSec: Double); override;
   end;
 
 implementation
@@ -61,6 +64,10 @@ begin
 end;
 
 procedure TTest7.OnPaint(Canvas: TCanvas; Left,Top,Width, Height: Integer);
+var
+  i: Integer;
+  smallW,smallH: integer;
+  pixBmp: TBGRABitmap;
 begin
   if (virtualscreen <> nil) and ((virtualscreen.width <> width) or (virtualscreen.Height <> height)) then
   begin
@@ -68,16 +75,44 @@ begin
     FreeAndNil(background);
   end;
 
+  smallW := (Width+4) div 8;
+  smallH := (Height+4) div 8;
+
   if virtualscreen = nil then
   begin
     virtualscreen := TBGRABitmap.Create(Width,Height);
     background := TBGRABitmap.Create(Width,Height);
     background.GradientFill(0,0,Width,Height,multigrad,gtRadial,PointF(0,0),PointF(Width/2,Height/2),dmSet,False);
+
+    setlength(pixels, round(sqrt(smallW*smallH)) );
+    for i := 0 to high(pixels) do
+    begin
+      pixels[i].x := random(smallW);
+      pixels[i].y := random(smallH);
+      pixels[i].dx := (random(1001)-500)/500;
+      pixels[i].dy := (random(1001)-500)/500;
+    end;
   end;
 
   virtualscreen.PutImage(0,0,background,dmSet);
+
+  pixBmp := TBGRABitmap.Create(smallW, smallH);
+  for i := 0 to high(pixels) do
+  begin
+    pixBmp.DrawPixelF(pixels[i].x, pixels[i].y, BGRABlack, dmDrawWithTransparency);
+    IncF(pixels[i].x, pixels[i].dx);
+    IncF(pixels[i].y, pixels[i].dy);
+    if pixels[i].x <= -1 then pixels[i].x := smallW else
+    if pixels[i].x >= smallW then pixels[i].x := -1;
+    if pixels[i].y <= -1 then pixels[i].y := smallH else
+    if pixels[i].y >= smallH then pixels[i].y := -1;
+  end;
+  virtualScreen.StretchPutImage(rect(0,0,Width,Height), pixBmp, dmDrawWithTransparency);
+  pixBmp.Free;
+
   DrawPacman(virtualScreen.Width div 4,virtualScreen.Height div 2, ln(time+1)*100);
   DrawPacman(3*virtualScreen.Width div 4,virtualScreen.Height div 2, -time*4);
+
   virtualscreen.Draw(Canvas,Left,Top,True);
 end;
 
