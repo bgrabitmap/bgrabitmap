b521f58535b4f18367c2d4cbd672725e8d8ab9fe
Johann ELSASS <circular@operamail.com>
2024-02-27 18:39:13 +0100

    generate chart with bash

--- doc/generate.sh
+++ doc/generate.sh
@@ -1,6 +1,15 @@
-if [ ! -d "web" ]; then
-	mkdir web
+DOCSPATH=web
+if [ ! -d "$DOCSPATH" ]; then
+	mkdir $DOCSPATH
 fi
 echo Removing previously generated pages...
-rm web/*.html
-./pasdoc @pasdoc.cfg --graphviz-uses --link-gv-uses=svg --graphviz-classes --link-gv-classes=svg
+rm $DOCSPATH/*.html
+rm $DOCSPATH/*.dot
+rm $DOCSPATH/*.svg
+
+./pasdoc @pasdoc.cfg -X --graphviz-uses --link-gv-uses=svg --graphviz-classes --link-gv-classes=svg
+
+perl -ni -e 'print unless /"(SysUtils|Classes|BGRAClasses|BGRABitmapTypes|Math|Types|FPImgCanv|FPImage|fgl)"/i' $DOCSPATH/GVUses.dot
+
+dot -Grankdir=LR -T svg $DOCSPATH/GVUses.dot > $DOCSPATH/GVUses.svg
+dot -Grankdir=LR -T svg $DOCSPATH/GVClasses.dot > $DOCSPATH/GVClasses.svg
