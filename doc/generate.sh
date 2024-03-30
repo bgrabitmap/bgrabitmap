6dbfee8657c49f05d9889861914abd45b12693ef
Juliette ELSASS <circular@fastmail.com>
2024-03-30 08:58:36 +0100

    generate graph on Windows

--- doc/generate.sh
+++ doc/generate.sh
@@ -1,19 +1,31 @@
 DOCSPATH=web
 if [ ! -d "$DOCSPATH" ]; then
 	mkdir $DOCSPATH
+else
+	echo Removing previously generated pages...
+	rm $DOCSPATH/*.html
+	rm $DOCSPATH/*.dot
+	rm $DOCSPATH/*.svg
+	if [ -d "$DOCSPATH/tipuesearch" ]; then
+		rm $DOCSPATH/tipuesearch/*
+	fi
 fi
-echo Removing previously generated pages...
-rm $DOCSPATH/*.html
-rm $DOCSPATH/*.dot
-rm $DOCSPATH/*.svg
 
 ./pasdoc @pasdoc.cfg -X --graphviz-uses --link-gv-uses=svg --graphviz-classes --link-gv-classes=svg
 
 echo Generating graphs...
+testcmd () {
+    command -v "$1" >/dev/null
+}
+if testcmd dot; then
+	DOT="dot"
+else
+	DOT="./dot.bat"
+fi
 perl -ni -e 'print unless /"(SysUtils|Classes|BGRAGraphics|BGRAClasses|BGRABitmapTypes|Math|Types|FPImgCanv|FPImage|fgl)"/i' $DOCSPATH/GVUses.dot
 perl -ni -e 'print unless /"(TObject)"/i' $DOCSPATH/GVClasses.dot
-dot -Grankdir=LR -T svg $DOCSPATH/GVUses.dot > $DOCSPATH/GVUses.svg
-dot -Grankdir=LR -T svg $DOCSPATH/GVClasses.dot > $DOCSPATH/GVClasses.svg
+$DOT -Grankdir=LR -T svg $DOCSPATH/GVUses.dot > $DOCSPATH/GVUses.svg
+$DOT -Grankdir=LR -T svg $DOCSPATH/GVClasses.dot > $DOCSPATH/GVClasses.svg
 
 echo Formatting HTML...
 cp navigation.js $DOCSPATH
