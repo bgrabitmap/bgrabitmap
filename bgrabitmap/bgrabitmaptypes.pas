5ab93359581406e18a718521262aa7b3989d0906
Juliette ELSASS <circular@fastmail.com>
2025-03-11 10:35:48 +0100

    version 11.6.4

--- bgrabitmap/bgrabitmaptypes.pas
+++ bgrabitmap/bgrabitmaptypes.pas
@@ -27,7 +27,7 @@ uses
 {=== Miscellaneous types ===}
 
   {* Current version expressed as an integer with each part multiplied by 100 }
-  const BGRABitmapVersion = 11060300;
+  const BGRABitmapVersion = 11060400;
 
   {* String representation of the version, numbers separated by dots }
   function BGRABitmapVersionStr: string;
