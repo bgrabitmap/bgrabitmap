d36887caef9a46188f74ab3735354cc9c18d6ee2
Massimo Magnano <maxm.dev@gmail.com>
2025-03-24 13:55:32 +0100

    Writer BMP Added GrayScale property and Published properties; PNG Writer Changed property Visibility to published
    Writer BMP Added GrayScale property and Published properties;
    PNG Writer Changed property Visibility to published

--- bgrabitmap/bgrawritebmp.pas
+++ bgrabitmap/bgrawritebmp.pas
@@ -14,21 +14,43 @@ interface
 
 uses SysUtils, Classes, FPImage, FPWriteBMP, BGRABitmapTypes;
 
+const
+   BMP_BitsValidValues: array[0..6] of byte = (1,4,8,15,16,24,32);
+
 type
-  {* Extends the TFPWriterBMP to save resolution }
+  { TBGRAWriterBMP }
+
   TBGRAWriterBMP = class (TFPWriterBMP)
   protected
-    {$IF FPC_FULLVERSION<30203}
+    FGrayScale: boolean;
+
+    procedure SetGrayScale(AValue: boolean); virtual;
+
     function SaveHeader(Stream:TStream; Img: TFPCustomImage):boolean; override;
-    {$ENDIF}
+    procedure InternalWrite(Stream:TStream; Img: TFPCustomImage); override;
+
+  public
+    constructor Create; override;
+
+  published
+    property GrayScale: boolean read FGrayscale write SetGrayScale;
+    property BitsPerPixel;  //: byte rw [1,4,8,15,16,24,32];
+    property RLECompress;   //: boolean rw;
   end;
 
 
 implementation
 
-{$IF FPC_FULLVERSION<30203}
+procedure TBGRAWriterBMP.SetGrayScale(AValue: boolean);
+begin
+  if FGrayscale=AValue then Exit;
+  FGrayscale:=AValue;
+  BitsPerPixel:= 8;
+end;
+
 function TBGRAWriterBMP.SaveHeader(Stream:TStream; Img : TFPCustomImage):boolean;
 begin
+  {$IF FPC_FULLVERSION<30203}
   if (Img is TCustomUniversalBitmap) then
   with TCustomUniversalBitmap(Img) do
   begin
@@ -36,10 +58,37 @@ begin
     Self.XPelsPerMeter :=Trunc(ResolutionX*100);
     Self.YPelsPerMeter :=Trunc(ResolutionY*100);
   end;
+  {$ENDIF}
 
-  Result:=Inherited SaveHeader(Stream, Img);
+  Result:= inherited SaveHeader(Stream, Img);
 end;
-{$ENDIF}
+
+procedure TBGRAWriterBMP.InternalWrite(Stream: TStream; Img: TFPCustomImage);
+var
+   newPal: TFPPalette;
+
+begin
+  if FGrayscale then
+  begin
+    try
+       newPal:= CreateGrayScalePalette;
+       Img.UsePalette:= True;
+       Img.Palette.Copy(newPal);
+       newPal.Free;
+    except
+       newPal.Free;
+    end;
+  end;
+
+  inherited InternalWrite(Stream, Img);
+end;
+
+constructor TBGRAWriterBMP.Create;
+begin
+  inherited Create;
+
+  FGrayScale:= False;
+end;
 
 initialization
   BGRARegisterImageWriter(ifBMP, TBGRAWriterBMP, True, 'BMP Format', 'bmp');
